8cd2f07b1a0b39e2efc5e7caf29da37a6d4fef4f
Emir Puska <emir.puska@it-akademija.com>
2024-11-05 20:41:15 +0100

    Ovo je nasa inicijalna verzija

---
+++ src/DrugiProgram.java
@@ -0,0 +1,2 @@
+public class DrugiProgram {
+}
---
+++ src/one/Program.java
@@ -0,0 +1,13 @@
+package one;
+
+//psvm
+//sout
+//CTRL + ALT + L
+//CTRL + SHIFT + (Fn)F10
+public class Program {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Vozdra raja šta ima haaa");
+        System.out.println("Kako ste mi danas ?");
+    }
+}
